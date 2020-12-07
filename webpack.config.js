3a687fa213b1b92f8eab9432ba5e8791bbf8e77b
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-07 13:47:48 +0900

    reactの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,19 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.ts", // 最初に読み込むファイル
+  entry: "./src/index.tsx", // 最初に読み込むファイル
   module: {
     rules: [
       // build時に追加で行う処理
       {
-        test: /\.ts$/, // testのファイルに対して、useのツールを実行
+        test: /\.tsx?$/, // testのファイルに対して、useのツールを実行 tsx? xは任意
         use: "ts-loader",
         exclude: /node_modules/, // 除外
       },
     ],
   },
   resolve: {
-    extensions: [".ts"], // モジュールとして解決する拡張子
+    extensions: [".js", ".ts", "tsx"], // モジュールとして解決する拡張子(外部ライブラリを使うファイル)
   },
   output: {
     // 出力するファイルの設定
