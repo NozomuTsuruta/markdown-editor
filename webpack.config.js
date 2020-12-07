1e7add9f4d3f3dd8fcd74fdb663c1d4ef5041dcf
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-07 13:36:26 +0900

    typescriptの導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js", // 最初に読み込むファイル
-  output: { // 出力するファイルの設定
+  entry: "./src/index.ts", // 最初に読み込むファイル
+  module: {
+    rules: [
+      // build時に追加で行う処理
+      {
+        test: /\.ts$/, // testのファイルに対して、useのツールを実行
+        use: "ts-loader",
+        exclude: /node_modules/, // 除外
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts"], // モジュールとして解決する拡張子
+  },
+  output: {
+    // 出力するファイルの設定
     path: path.resolve(__dirname, "dist"), // 出力するディレクトリ
     filename: "index.js", // 出力するファイル
     publicPath: "dist/", // 変換する時に相対パスにdist/を追加
