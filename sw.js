5d97aac59dd45f5f7556f65f91b76a2c3fbfe887
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-08 19:49:11 +0900

    manifest導入

--- sw.js
+++ sw.js
@@ -1,8 +1,27 @@
 // selfはserviceWorkerをさす
+const CacheName = "Cache:v1";
+
 self.addEventListener("install", (event) => {
   console.log("ServiceWorker install:", event);
 });
 
 self.addEventListener("activate", (event) => {
   console.log("ServiceWorker activate:", event);
+});
+
+const networkFallingBackToCache = async (request) => {
+  const cache = await caches.open(CacheName); // 定義した名前でキャッシュを開く
+  try {
+    const response = await fetch(request); // リクエストに対してレスポンスを保持
+    await cache.put(request, response.clone()); // responseをコピーして(一度しか読み取れないものがあるから)、キャッシュに保存
+    return response;
+  } catch (err) {
+    console.error(err);
+    return cache.match(request);
+  }
+};
+
+self.addEventListener("fetch", (event) => {
+  // fetchに介入
+  event.respondWith(networkFallingBackToCache(event.request));
 });
